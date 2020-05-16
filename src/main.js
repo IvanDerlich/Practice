5e832a79d916dc125e1d30983e7ade0e6a357e99
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-05-15 21:38:31 -0300

    adsf

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 // https://jestjs.io/docs/en/getting-started.html#content
 
+// https://jestjs.io/docs/en/using-matchers.html#content
+
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,3 +1,5 @@
+
+
 const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
@@ -65,4 +67,17 @@ const shoppingList = [
 test('the shopping list has beer on it', () => {
   expect(shoppingList).toContain('beer');
   expect(new Set(shoppingList)).toContain('beer');
+});
+
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK');
+}
+
+test('compiling android goes as expected', () => {
+  expect(compileAndroidCode).toThrow();
+  expect(compileAndroidCode).toThrow(Error);
+
+  // You can also use the exact error message or a regexp
+  expect(compileAndroidCode).toThrow('you are using the wrong JDK');
+  expect(compileAndroidCode).toThrow(/JDK/);
 });
